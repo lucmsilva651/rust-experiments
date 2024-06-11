0f36df7f9c24e62893824ee554ed3afaeb8efa60
lucmsilva651 <lucmsilva651@gmail.com>
2024-06-11 19:16:05 -0300

    primeiro commit foda

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+// teste comentarios
+
+/*
+teste comentarios
+multilinha
+*/
+
+/*
+rust é muito facil porra
+*/
+
+// helloworld fodao ai pra vcs
+// compila com rustc .\src\main.rs (powershell lore)
+fn main() {
+  println!("Hello World!\nAqui é o teste de Rust")
+}
